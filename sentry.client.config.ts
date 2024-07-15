7443114f1c2a31bc01ca1439a118d61a432094e2
anshshah23 <ANSH.SHAH082@svkmmumbai.onmicrosoft.com>
2024-07-15 16:22:08 +0530

    C

--- sentry.client.config.ts
+++ sentry.client.config.ts
@@ -5,7 +5,7 @@
 import * as Sentry from "@sentry/nextjs";
 
 Sentry.init({
-  dsn: "https://7b883d075caa19b41fd9b00ae313a1c6@o4506813739368448.ingest.us.sentry.io/4507222371729408",
+  dsn: "https://9c197438d1717fc9fa5ab942392cbd88@o4507605416673280.ingest.de.sentry.io/4507605424537680",
 
   // Adjust this value in production, or use tracesSampler for greater control
   tracesSampleRate: 1,
