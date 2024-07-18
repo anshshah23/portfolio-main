23635dfe14051f5e0797fe9eeccc014eb46d2401
Ansh Shah <122973942+anshshah23@users.noreply.github.com>
2024-07-18 18:24:16 +0530

    Update index.ts

--- data/index.ts
+++ data/index.ts
@@ -52,7 +52,7 @@ export const gridItems = [
 
   {
     id: 5,
-    title: "Currently learning Next.js",
+    title: "Currently working on a Mental Healthcare Website",
     description: "The Inside Scoop",
     className: "md:col-span-3 md:row-span-2",
     imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
